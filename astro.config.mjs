654daabc647e2c7b1345685a9005860559cb7806
David Huh <github@davidhuh>
2025-03-05 19:04:33 +0100

    fix: update base path in astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://huh-david.github.io",
+  base: "blog",
   integrations: [mdx(), sitemap()],
 
   vite: {
